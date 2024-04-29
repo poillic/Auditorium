51d3788f5f2281587b17839cc96c94ff1a9985d9
Proxyma <oillic.p@gmail.com>
2024-04-29 14:05:47 +0200

    feat(MusicBox) : Gestion du volume et des barres

--- Auditorium/Assets/Scripts/MusicBox.cs
+++ Auditorium/Assets/Scripts/MusicBox.cs
@@ -19,5 +19,29 @@ public class MusicBox : MonoBehaviour
     void Update()
     {
         // En fonction de la propriété _audioSource.volume le bon nombre de barre s'allume
+
+        for ( int i = 0; i < _bars.Length; i++ )
+        {
+            float seuil = (float)i / (float)_bars.Length;
+            /*
+             * 0 / 5 = 0
+             * 1 / 5 = 0.2
+             * 2 / 5 = 0.4
+             * 3 / 5 = 0.6
+             * 4 / 5 = 0.8
+             * 5 / 5 = 1
+             * 
+             */
+            if ( _audioSource.volume > seuil )
+            {
+                // on allume la barre
+                _bars[ i ].color = _onColor;
+            }
+            else
+            {
+                // On éteinds la barre
+                _bars[ i ].color = _offColor;
+            }
+        }
     }
 }
