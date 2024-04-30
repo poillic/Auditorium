87b9f2ad8e99d4056dc36ad2f274889f5be8120d
Proxyma <oillic.p@gmail.com>
2024-04-30 10:00:09 +0200

    feat(MouseManager) : Cursor style

--- Auditorium/Assets/Scripts/MouseManager.cs
+++ Auditorium/Assets/Scripts/MouseManager.cs
@@ -5,6 +5,9 @@ using UnityEngine.InputSystem;
 
 public class MouseManager : MonoBehaviour
 {
+
+    public Texture2D centerIcon;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +25,29 @@ public class MouseManager : MonoBehaviour
         Vector2 pointerPosition = context.ReadValue<Vector2>();
 
         _ray = Camera.main.ScreenPointToRay( pointerPosition );
+
+        RaycastHit2D hit = Physics2D.GetRayIntersection( _ray );
+
+        // Si j'ai touché quelque chose
+        if( hit.collider != null )
+        {
+            if( hit.collider.CompareTag("CenterZone") )
+            {
+                Debug.Log( "CENTER" );
+                Cursor.SetCursor( centerIcon, new Vector2( 256, 256 ), CursorMode.Auto );
+                // J'ai touché la fleche
+            }
+            else if( hit.collider.CompareTag( "OutterZone" ) )
+            {
+                // J'ai touché le cercle exterieur
+                Debug.Log( "OUTTER" );
+            }
+        }
+        else
+        {
+            //Notre pointer survole rien, on remet le curseur à sa forme par défaut
+            Cursor.SetCursor( null, Vector2.zero, CursorMode.Auto );
+        }
     }
 
     private void OnDrawGizmos()
